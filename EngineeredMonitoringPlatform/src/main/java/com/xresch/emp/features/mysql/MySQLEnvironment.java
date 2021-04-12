ecfd05c1fa63b6787d10f70379dfdaf3121e9843
Reto <reto.scheiwiller5@bluewin.ch>
2021-04-12 11:01:38 +0200

    added MySQL (draft)

---
+++ EngineeredMonitoringPlatform/src/main/java/com/xresch/emp/features/mysql/MySQLEnvironment.java
@@ -0,0 +1,139 @@
+package com.xresch.emp.features.mysql;
+
+import com.xresch.cfw._main.CFW;
+import com.xresch.cfw.datahandling.CFWField;
+import com.xresch.cfw.datahandling.CFWField.FormFieldType;
+import com.xresch.cfw.db.DBInterface;
+import com.xresch.cfw.features.contextsettings.AbstractContextSettings;
+import com.xresch.cfw.features.dashboard.DashboardWidget;
+import com.xresch.cfw.features.dashboard.DashboardWidget.DashboardWidgetFields;
+import com.xresch.cfw.response.bootstrap.AlertMessage.MessageType;
+
+/**************************************************************************************************************
+ * 
+ * @author Reto Scheiwiller, (c) Copyright 2021
+ * @license MIT-License
+ **************************************************************************************************************/
+public class MySQLEnvironment extends AbstractContextSettings {
+	
+	public static final String SETTINGS_TYPE = "MySQL Environment";
+	
+	private DBInterface dbInstance = null;
+	
+	public enum MySQLEnvironmentFields{
+		DB_HOST,
+		DB_PORT,
+		DB_NAME,
+		DB_USER,
+		DB_PASSWORD,
+	}
+		
+	private CFWField<String> dbHost = CFWField.newString(FormFieldType.TEXT, MySQLEnvironmentFields.DB_HOST)
+			.setDescription("The server name of the database host.");
+	
+	private CFWField<Integer> dbPort = CFWField.newInteger(FormFieldType.NUMBER, MySQLEnvironmentFields.DB_PORT)
+			.setDescription("The port used to access the database.");
+	
+	private CFWField<String> dbName = CFWField.newString(FormFieldType.TEXT, MySQLEnvironmentFields.DB_NAME)
+			.setDescription("The name of the user for accessing the database.");
+	
+	private CFWField<String> dbUser = CFWField.newString(FormFieldType.TEXT, MySQLEnvironmentFields.DB_USER)
+			.setDescription("The name of the user for accessing the database.");
+	
+	private CFWField<String> dbPassword = CFWField.newString(FormFieldType.PASSWORD, MySQLEnvironmentFields.DB_PASSWORD)
+			.setDescription("The password of the DB user.")
+			.disableSecurity()
+			.enableEncryption("mysql_DB_PW_Salt");
+	
+	
+	public MySQLEnvironment() {
+		initializeFields();
+	}
+		
+	private void initializeFields() {
+		this.addFields(dbHost, dbPort, dbName, dbUser, dbPassword);
+	}
+		
+	
+	@Override
+	public boolean isDeletable(int id) {
+		
+		int count = new DashboardWidget()
+			.selectCount()
+			.whereLike(DashboardWidgetFields.JSON_SETTINGS, "%\"environment\":"+id+"%")
+			.and().like(DashboardWidgetFields.TYPE, "emp_mysql%")
+			.getCount();
+		
+		if(count == 0) {
+			return true;
+		}else {
+			CFW.Context.Request.addAlertMessage(MessageType.ERROR, "The MySQL Environment cannot be deleted as it is still in use by "+count+"  widget(s).");
+			return false;
+		}
+
+	}
+	
+	public boolean isDBDefined() {
+		if(dbHost.getValue() != null
+		&& dbPort.getValue() != null
+		&& dbUser.getValue() != null) {
+			return true;
+		}
+		
+		return false;
+	}
+			
+	public String dbHost() {
+		return dbHost.getValue();
+	}
+	
+	public MySQLEnvironment dbHost(String value) {
+		this.dbHost.setValue(value);
+		return this;
+	}
+		
+	public int dbPort() {
+		return dbPort.getValue();
+	}
+	
+	public MySQLEnvironment dbPort(int value) {
+		this.dbPort.setValue(value);
+		return this;
+	}
+	
+	public String dbName() {
+		return dbName.getValue();
+	}
+	
+	public MySQLEnvironment dbName(String value) {
+		this.dbName.setValue(value);
+		return this;
+	}
+	
+	public String dbUser() {
+		return dbUser.getValue();
+	}
+	
+	public MySQLEnvironment dbUser(String value) {
+		this.dbUser.setValue(value);
+		return this;
+	}
+	
+	public String dbPassword() {
+		return dbPassword.getValue();
+	}
+	
+	public MySQLEnvironment dbPassword(String value) {
+		this.dbPassword.setValue(value);
+		return this;
+	}	
+
+	public DBInterface getDBInstance() {
+		return dbInstance;
+	}
+
+	public void setDBInstance(DBInterface dbInstance) {
+		this.dbInstance = dbInstance;
+	}
+	
+}
---
+++ EngineeredMonitoringPlatform/src/main/java/com/xresch/emp/features/mysql/resources/emp_widget_mysqlquerystatus.js
@@ -0,0 +1,144 @@
+(function (){
+	
+	/******************************************************************
+	 * 
+	 ******************************************************************/
+	CFW.dashboard.registerCategory("fas fa-database", "Monitoring | MySQL");
+	
+	/******************************************************************
+	 * 
+	 ******************************************************************/
+	CFW.dashboard.registerWidget("emp_mysqlquerystatus",
+		{
+			category: "Monitoring | MySQL",
+			menuicon: "fas fa-thermometer-half",
+			menulabel: CFWL('emp_widget_mysqlquerystatus', "Query Status"),
+			description: CFWL('emp_widget_mysqlquerystatus', "Executes an SQL Query and displays a threshhold based status."), 
+			usetimeframe: true,
+			createWidgetInstance: function (widgetObject, callback) {
+					
+				CFW.dashboard.fetchWidgetData(widgetObject, function(data){
+					
+					var settings = widgetObject.JSON_SETTINGS;
+					var renderType = (settings.renderer == null) ? "tiles" : settings.renderer.toLowerCase() ;
+					console.log(settings);
+					
+					//---------------------------------
+					// Check for Data and Errors
+					if(CFW.utils.isNullOrEmpty(data.payload) || typeof data.payload == 'string' || data.payload.length == 0){
+						callback(widgetObject, '');
+						return;
+					}
+					
+					//---------------------------------
+					// Use last Column if not specified
+					var valueColumn = settings.valuecolumn;
+					if(CFW.utils.isNullOrEmpty(valueColumn)){
+						let keys = Object.keys(data.payload[0]);
+						valueColumn = keys[keys.length-1];
+					}
+					
+					//---------------------------------
+					// Prepare TitleFields
+					var labelColumns = settings.labelcolumns;
+					var titlefields = null;
+					if(!CFW.utils.isNullOrEmpty(labelColumns)){
+						titlefields = labelColumns.trim().split(/ ?, ?/g);
+					}
+					
+					//---------------------------------
+					// Prepare visible fields
+					var visiblefields = Object.keys(data.payload[0]);
+					var detailColumns = settings.detailcolumns;
+					
+					if(!CFW.utils.isNullOrEmpty(detailColumns)){
+						visiblefields = detailColumns.trim().split(/ ?, ?/g);
+					}
+					console.log("valueColumn:"+valueColumn);
+					console.log("detailColumns:"+detailColumns);
+					console.log("labelColumns:"+labelColumns);
+					console.log("titlefields:"+titlefields);
+					console.log("visiblefields:"+visiblefields);
+					
+					//---------------------------
+					// Set Colors for Thresholds
+					var excellentVal = settings.threshold_excellent;
+					var goodVal = settings.threshold_good;
+					var warningVal = settings.threshold_warning;
+					var emergencyVal = settings.threshold_emergency;
+					var dangerVal = settings.threshold_danger;
+					
+					for(var key in data.payload){
+						var current = data.payload[key];
+						
+						current.alertstyle =  CFW.colors.getThresholdStyle(current[valueColumn]
+								,excellentVal
+								,goodVal
+								,warningVal
+								,emergencyVal
+								,dangerVal
+								,settings.disable);
+						
+						if(current.alertstyle != "cfw-none"){
+							current.textstyle = "white"; 
+						}
+					}
+					
+					//---------------------------
+					// Render Settings
+					var dataToRender = {
+						data: data.payload,
+						bgstylefield: 'alertstyle',
+						textstylefield: 'textstyle',
+						titlefields: titlefields, 
+						visiblefields: visiblefields,
+						titleformat: null, 
+						
+						labels: {},
+						customizers: {
+							value: function(record, value) {
+								if(value == null) return '';
+								return (settings.suffix == null) ? value : value+" "+settings.suffix;
+							},
+							time: function(record, value) { return (value != null) ? new  moment(value).format("YYYY-MM-DD HH:mm") : '';},
+						},
+						rendererSettings:{
+							tiles: {
+								sizefactor: settings.sizefactor,
+								showlabels: settings.showlabels,
+								borderstyle: settings.borderstyle
+							},
+							table: {
+								narrow: 	true,
+								striped: 	false,
+								hover: 		false,
+								filterable: false,
+							},
+							panels: {
+								narrow: 	true,
+							},
+							cards: {
+								narrow: 	true,
+								maxcolumns: 5,
+							},
+					}};
+										
+					//--------------------------
+					// Render Widget
+					var renderer = CFW.render.getRenderer(renderType);
+					callback(widgetObject, renderer.render(dataToRender));
+				});
+			},
+			
+			getEditForm: function (widgetObject) {
+				return CFW.dashboard.getSettingsForm(widgetObject);
+			},
+			
+			onSave: function (form, widgetObject) {
+				widgetObject.JSON_SETTINGS = CFW.format.formToObject(form);
+				return true;	
+			}
+		}
+	);	
+	
+})();;
