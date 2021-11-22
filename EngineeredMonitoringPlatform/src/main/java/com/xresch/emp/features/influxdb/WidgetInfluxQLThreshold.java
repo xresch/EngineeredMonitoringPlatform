0fc004f7aea6885bfc501cbcc5efa4c15ab452cb
Reto <reto.scheiwiller5@bluewin.ch>
2021-11-22 16:07:25 +0100

    added task to InfluxQL Threshold Widget

--- EngineeredMonitoringPlatform/src/main/java/com/xresch/emp/features/influxdb/WidgetInfluxQLThreshold.java
+++ EngineeredMonitoringPlatform/src/main/java/com/xresch/emp/features/influxdb/WidgetInfluxQLThreshold.java
@@ -1,15 +1,22 @@
 package com.xresch.emp.features.influxdb;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Locale;
+import java.util.Map.Entry;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
 import com.google.common.base.Strings;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.xresch.cfw._main.CFW;
 import com.xresch.cfw.caching.FileDefinition;
@@ -17,20 +24,26 @@ import com.xresch.cfw.caching.FileDefinition.HandlingType;
 import com.xresch.cfw.datahandling.CFWField;
 import com.xresch.cfw.datahandling.CFWField.FormFieldType;
 import com.xresch.cfw.datahandling.CFWObject;
+import com.xresch.cfw.features.dashboard.DashboardWidget;
 import com.xresch.cfw.features.dashboard.WidgetDefinition;
 import com.xresch.cfw.features.dashboard.WidgetSettingsFactory;
 import com.xresch.cfw.features.jobs.CFWJobsAlertObject;
+import com.xresch.cfw.features.jobs.CFWJobsAlertObject.AlertType;
 import com.xresch.cfw.features.usermgmt.User;
 import com.xresch.cfw.logging.CFWLog;
 import com.xresch.cfw.response.JSONResponse;
 import com.xresch.cfw.response.bootstrap.AlertMessage.MessageType;
+import com.xresch.cfw.utils.CFWConditions.ThresholdCondition;
 import com.xresch.cfw.validation.NotNullOrEmptyValidator;
+import com.xresch.cfw.validation.NumberRangeValidator;
 
 public class WidgetInfluxQLThreshold extends WidgetDefinition {
 
+	private static final String FIELDNAME_LABELS = "labels";
+	private static final String FIELDNAME_VALUECOLUMN = "valuecolumn";
 	private static final String FIELDNAME_SUFFIX = "suffix";
-	private static final String FIELDNAME_QUERY = "query";
 	private static final String FIELDNAME_ALERT_THRESHOLD = "ALERT_THRESHOLD";
+	private static final String FIELDNAME_ALERT_TIMESPAN_MINUTES = "TIMESPAN_MINUTES";
 	
 	private static Logger logger = CFWLog.getLogger(WidgetInfluxQLThreshold.class.getName());
 	@Override
@@ -62,13 +75,12 @@ public class WidgetInfluxQLThreshold extends WidgetDefinition {
 					)
 				)
 			
-				.addField(CFWField.newString(FormFieldType.TEXT, "valuecolumn")
+				.addField(CFWField.newString(FormFieldType.TEXT, FIELDNAME_VALUECOLUMN)
 						.setLabel("{!emp_widget_influxdb_influxql_valuecolumn!}")
 						.setDescription("{!emp_widget_influxdb_influxql_valuecolumn_desc!}")
-						.addValidator(new NotNullOrEmptyValidator())
 				)
 				
-				.addField(CFWField.newString(FormFieldType.TEXT, "labels")
+				.addField(CFWField.newString(FormFieldType.TEXT, FIELDNAME_LABELS)
 						.setLabel("{!emp_widget_influxdb_influxql_labels!}")
 						.setDescription("{!emp_widget_influxdb_influxql_labels_desc!}")
 				)
@@ -198,7 +210,7 @@ public class WidgetInfluxQLThreshold extends WidgetDefinition {
 	 * 
 	 *********************************************************************/
 	public boolean supportsTask() {
-		return false;
+		return true;
 	}
 	
 	/************************************************************
@@ -206,7 +218,7 @@ public class WidgetInfluxQLThreshold extends WidgetDefinition {
 	 * task of this widget does.
 	 ************************************************************/
 	public String getTaskDescription() {
-		return "Checks if any of the selected values returned by the instant query exceeds the selected threshold.";
+		return "Checks if the latest values returned by the InfluxQL query exceed the selected threshold.";
 	}
 	
 	/************************************************************
@@ -225,6 +237,12 @@ public class WidgetInfluxQLThreshold extends WidgetDefinition {
 					.addValidator(new NotNullOrEmptyValidator())
 					.setOptions(CFW.Conditions.CONDITION_OPTIONS())
 					.setValue(CFW.Conditions.CONDITION_EMERGENCY.toString())
+				)
+				.addField(
+					CFWField.newInteger(FormFieldType.NUMBER, FIELDNAME_ALERT_TIMESPAN_MINUTES)
+					.setDescription("Select the timespan to check. For example, 60 minutes will query a timespan between present time and 60 minutes in the past.")
+					.addValidator(new NumberRangeValidator(5, 60*24))
+					.setValue(60)
 				);
 	}
 	
@@ -233,138 +251,226 @@ public class WidgetInfluxQLThreshold extends WidgetDefinition {
 	 * See {@link com.xresch.cfw.features.jobs.CFWJobTask#executeTask CFWJobTask.executeTask()} to get
 	 * more details on how to implement this method.
 	 *************************************************************************/
-//	public void executeTask(JobExecutionContext context, CFWObject taskParams, DashboardWidget widget, CFWObject settings) throws JobExecutionException {
-//		
-//		//----------------------------------------
-//		// Fetch Data
-//		JsonObject payloadObject;
-//		Boolean isSampleData = (Boolean)settings.getField(WidgetSettingsFactory.FIELDNAME_SAMPLEDATA).getValue();
-//		if(isSampleData != null && isSampleData) {
-//			payloadObject = createSampleData();
-//		}else {
-//			payloadObject = loadDataFromInfluxDB(settings, System.currentTimeMillis() , System.currentTimeMillis());
-//		}
-//		
-//		if(payloadObject == null || payloadObject.size() == 0) {
-//			return;
-//		}
-//		
-//		//----------------------------------------
-//		// Get alertThreshhold
-//		String alertThreshholdString = (String)taskParams.getField(FIELDNAME_ALERT_THRESHOLD).getValue();
-//		
-//		if(alertThreshholdString == null ) {
-//			return;
-//		}
-//
-//		ThresholdCondition alertThreshholdCondition = ThresholdCondition.valueOf(alertThreshholdString);
-//		
-//		//----------------------------------------
-//		// Get Results
-//		JsonObject payloadObject = payloadObject.get(0).getAsJsonObject();
-//		
-//		JsonElement dataObject = payloadObject.get("data");
-//		if(dataObject == null || !dataObject.isJsonObject()) {
-//			return;
-//		}
-//		
-//		JsonElement resultElement = dataObject.getAsJsonObject().get("result");
-//		if(resultElement == null || !resultElement.isJsonArray()) {
-//			return;
-//		}
-//		
-//		JsonArray resultArray = resultElement.getAsJsonArray();
-//		
-//		//----------------------------------------
-//		// Check Condition
-//		boolean conditionMatched = false;
-//		ArrayList<JsonObject> instantExceedingThreshold = new ArrayList<>();
-//		
-//		for(JsonElement element : resultArray) {
-//			
-//			
-//			JsonObject current = element.getAsJsonObject();
-//			JsonArray valueArray = current.get("value").getAsJsonArray();
-//			Float value = valueArray.get(1).getAsFloat();
-//			ThresholdCondition condition = CFW.Conditions.getConditionForValue(value, settings);
-//			if(condition != null 
-//			&& CFW.Conditions.compareIsEqualsOrMoreDangerous(alertThreshholdCondition, condition)) {
-//				conditionMatched = true;
-//				instantExceedingThreshold.add(current);
-//			}
-//		}
-//				
-//		//----------------------------------------
-//		// Handle Alerting
-//		CFWJobsAlertObject alertObject = new CFWJobsAlertObject(context, this.getWidgetType());
-//
-//		alertObject.mapJobExecutionContext(context);
-//
-//		AlertType type = alertObject.checkSendAlert(conditionMatched, null);
-//		
-//		if(!type.equals(AlertType.NONE)) {
-//
-//			//----------------------------------------
-//			// Prepare Contents
-//			String linkHTML = "";
-//			if(widget != null) {
-//				linkHTML = widget.createWidgetOriginMessage();
-//			}
-//			
-//			//----------------------------------------
-//			// RAISE
-//			if(type.equals(AlertType.RAISE)) {
-//				
-//				//----------------------------------------
-//				// Create Job List 
-//				String metricListText = "";
-//				String metricListHTML = "<ul>";
-//				for(JsonObject current : instantExceedingThreshold) {
-//					
-//					JsonObject metricObject = current.get("metric").getAsJsonObject();
-//					
-//					//-----------------------------
-//					// Create Metric String
-//					String metricString = "";
-//					for(Entry<String, JsonElement> entry : metricObject.entrySet()) {
-//						
-//						metricString += entry.getKey()+": "+entry.getValue().toString()+", ";
-//					}
-//					metricString = metricString.substring(0, metricString.length()-2);
-//					
-//					
-//					//-----------------------------
-//					// Add to Lists
-//					metricListText +=  metricString+" / ";
-//					metricListHTML += "<li>"+metricString+"</li>";
-//
-//				}
-//				
-//				metricListText = metricListText.substring(0, metricListText.length()-3);
-//				metricListHTML+="</ul>";
-//				
-//				//----------------------------------------
-//				// Create Message
-//				String baseMessage = "The following metrics(s) have reached the threshold "+alertThreshholdString+":";
-//				String messagePlaintext = baseMessage+" "+metricListText;
-//				String messageHTML = "<p>"+baseMessage+"</p>";
-//				messageHTML += metricListHTML;
-//				messageHTML += linkHTML;
-//				
-//				CFW.Messages.addErrorMessage(messagePlaintext);
-//				
-//				alertObject.doSendAlert(context, MessageType.ERROR, "EMP: Alert - InfluxDB metric(s) reached threshold", messagePlaintext, messageHTML);
-//			}
-//			
-//			//----------------------------------------
-//			// RESOLVE
-//			if(type.equals(AlertType.RESOLVE)) {
-//				String message = CFW.Random.randomIssueResolvedMessage();
-//				String messageHTML = "<p>"+message+"</p>"+linkHTML;
-//				
-//				CFW.Messages.addSuccessMessage("Issue has resolved.");
-//				alertObject.doSendAlert(context, MessageType.SUCCESS, "EMP: Resolved - InfluxDB metric(s) below threshold", message, messageHTML);
-//			}
-//		}
-//	}
+	public void executeTask(JobExecutionContext context, CFWObject taskParams, DashboardWidget widget, CFWObject settings) throws JobExecutionException {
+		
+		//----------------------------------------
+		// Get and Check Settings
+		String valueColumnName = (String)settings.getField(FIELDNAME_VALUECOLUMN).getValue();
+		
+		if(Strings.isNullOrEmpty(valueColumnName)) {
+			CFW.Messages.addWarningMessage("Job not executed properly, value column is not specified.");
+			return;
+		}
+		//----------------------------------------
+		// Fetch Data
+		JsonObject payloadObject;
+		Boolean isSampleData = (Boolean)settings.getField(WidgetSettingsFactory.FIELDNAME_SAMPLEDATA).getValue();
+		if(isSampleData != null && isSampleData) {
+			payloadObject = createSampleData();
+		}else {
+			Integer timespanMinutes = (Integer)settings.getField(FIELDNAME_ALERT_TIMESPAN_MINUTES).getValue();
+			Integer timespanMillis = timespanMinutes * 60 * 1000;
+			
+			payloadObject = loadDataFromInfluxDB(settings, System.currentTimeMillis() - timespanMillis , System.currentTimeMillis());
+		}
+		
+		if(payloadObject == null || payloadObject.size() == 0) {
+			return;
+		}
+		
+		//----------------------------------------
+		// Get alertThreshhold
+		String alertThreshholdString = (String)taskParams.getField(FIELDNAME_ALERT_THRESHOLD).getValue();
+		
+		if(alertThreshholdString == null ) {
+			return;
+		}
+
+		ThresholdCondition alertThreshholdCondition = ThresholdCondition.valueOf(alertThreshholdString);
+		
+		//----------------------------------------
+		// Get Results
+		JsonArray resultArray = payloadObject.get("results").getAsJsonArray();
+
+		//----------------------------------------
+		// Check Condition
+		boolean conditionMatched = false;
+		ArrayList<Properties> valuesExceedingThreshold = new ArrayList<>();
+		
+		//----------------------------------------
+		// Iterate Statements in Results
+		
+		for(JsonElement statementElement : resultArray) {
+
+			JsonObject currentStatement = statementElement.getAsJsonObject();
+			JsonArray seriesArray = currentStatement.get("series").getAsJsonArray();
+			
+			//----------------------------------------
+			// Iterate Series in Statement
+			for(JsonElement seriesElement : seriesArray) {
+				
+				JsonObject currentSeries = seriesElement.getAsJsonObject();
+				JsonArray columnsArray = currentSeries.get("columns").getAsJsonArray();
+				JsonObject tagsObject = currentSeries.get("tags").getAsJsonObject();
+				
+				JsonArray valuesArray = currentSeries.get("values").getAsJsonArray();
+				
+				if(valuesArray.isJsonNull() || valuesArray.size() == 0) {
+					continue;
+					
+				}
+
+
+				//-------------------------------------
+				// Find Time Column Index
+				int timeIndex = 0;
+				for(; timeIndex < columnsArray.size(); timeIndex++) {
+					String columnName = columnsArray.get(timeIndex).getAsString();
+					if(columnName.equals("time")) {
+						break;
+					}
+				}
+				
+				//-------------------------------------
+				// Find Value Column Index
+				valueColumnName = valueColumnName.trim();
+				int valueIndex = 0;
+				for(; valueIndex < columnsArray.size(); valueIndex++) {
+					String columnName = columnsArray.get(valueIndex).getAsString();
+					if(columnName.equals(valueColumnName)) {
+						break;
+					}
+				}
+				
+				//-------------------------------------
+				// Make sure values are sorted by Time
+				// Descending
+				System.out.println("============== Before Sort ===============");
+				System.out.println(CFW.JSON.toJSON(valuesArray));
+				
+				final int finalTimeIndex = timeIndex;
+				
+
+				CFW.JSON.arraySortBy(valuesArray, 
+					new Comparator<JsonElement>() {
+						@Override
+						public int compare(JsonElement e1, JsonElement e2) {
+							long i1 = e1.getAsJsonArray().get(finalTimeIndex).getAsLong();
+						    long i2 = e2.getAsJsonArray().get(finalTimeIndex).getAsLong();
+						    return Long.compare(i1, i2);
+						}
+					}
+				);
+				
+				System.out.println("============== After Sort ===============");
+				System.out.println(CFW.JSON.toJSONPretty(valuesArray));
+				
+				
+				
+				//-------------------------------------
+				// Evaluate Threshold
+				JsonArray firstValues = valuesArray.get(0).getAsJsonArray();
+				
+				Float valueToCheck = firstValues.get(valueIndex).getAsFloat();
+				
+				ThresholdCondition condition = CFW.Conditions.getConditionForValue(valueToCheck, settings);
+				if(condition != null 
+				&& CFW.Conditions.compareIsEqualsOrMoreDangerous(alertThreshholdCondition, condition)) {
+					conditionMatched = true;
+					Properties data = new Properties();
+					data.put("values", firstValues);
+					data.put("tags", tagsObject);
+					data.put("columnNames", columnsArray);
+					valuesExceedingThreshold.add(data);
+				}
+
+			}
+		}
+				
+		//----------------------------------------
+		// Handle Alerting
+		CFWJobsAlertObject alertObject = new CFWJobsAlertObject(context, this.getWidgetType());
+
+		alertObject.mapJobExecutionContext(context);
+
+		AlertType type = alertObject.checkSendAlert(conditionMatched, null);
+		
+		if(!type.equals(AlertType.NONE)) {
+
+			//----------------------------------------
+			// Prepare Contents
+			String linkHTML = "";
+			if(widget != null) {
+				linkHTML = widget.createWidgetOriginMessage();
+			}
+			
+			//----------------------------------------
+			// RAISE
+			if(type.equals(AlertType.RAISE)) {
+				
+				//----------------------------------------
+				// Create Job List 
+				JsonArray metricListArray = new JsonArray();
+				String metricListHTML = "<div>";
+				
+				for(Properties current : valuesExceedingThreshold) {
+					
+					JsonArray values = (JsonArray)current.get("values");
+					JsonObject tags = (JsonObject)current.get("tags");
+					JsonArray columnNames = (JsonArray)current.get("columnNames");
+					
+					//-----------------------------
+					// Create Metric String
+					JsonObject metricObject = new JsonObject();
+					
+					//-----------------------------
+					// Iterate Tags
+					if(!tags.isJsonNull()) {
+						for( Entry<String, JsonElement> entry : tags.entrySet()) {
+							metricObject.add(entry.getKey(), entry.getValue());
+						}
+					}
+					
+					//-----------------------------
+					// Iterate Columns and Values
+					if(!columnNames.isJsonNull()) {
+						for(int j = 0; j < columnNames.size(); j++) {
+							metricObject.add(columnNames.get(j).getAsString(), values.get(j));
+						}
+					}
+					
+					//-----------------------------
+					// Add to Lists
+					metricListArray.add(metricObject);
+					metricListHTML += CFW.JSON.formatJsonObjectToHTMLTable(metricObject, true)+"</br></br>";
+
+				}
+				
+
+				metricListHTML+="</div>";
+				
+				//----------------------------------------
+				// Create Message
+				String baseMessage = "The following metrics(s) have reached the threshold "+alertThreshholdString+":";
+				String messagePlaintext = baseMessage+" "+metricListArray.toString();
+				String messageHTML = "<p>"+baseMessage+"</p>";
+				messageHTML += metricListHTML;
+				messageHTML += linkHTML;
+				
+				CFW.Messages.addErrorMessage(messagePlaintext);
+				
+				alertObject.doSendAlert(context, MessageType.ERROR, "EMP: Alert - InfluxDB metric(s) reached threshold", messagePlaintext, messageHTML);
+			}
+			
+			//----------------------------------------
+			// RESOLVE
+			if(type.equals(AlertType.RESOLVE)) {
+				String message = CFW.Random.randomIssueResolvedMessage();
+				String messageHTML = "<p>"+message+"</p>"+linkHTML;
+				
+				CFW.Messages.addSuccessMessage("Issue has resolved.");
+				alertObject.doSendAlert(context, MessageType.SUCCESS, "EMP: Resolved - InfluxDB metric(s) below threshold", message, messageHTML);
+			}
+		}
+	}
 }
