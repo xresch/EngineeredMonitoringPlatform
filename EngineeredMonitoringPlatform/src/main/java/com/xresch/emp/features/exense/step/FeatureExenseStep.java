58ce12fcf2c35c01c4d12d6a1f5fecfe5a226e04
Reto <reto.scheiwiller5@bluewin.ch>
2024-03-20 16:32:09 +0100

    Decommissioning Widget Scheduler Status All (Can be achieved using Status By User)

--- EngineeredMonitoringPlatform/src/main/java/com/xresch/emp/features/exense/step/FeatureExenseStep.java
+++ EngineeredMonitoringPlatform/src/main/java/com/xresch/emp/features/exense/step/FeatureExenseStep.java
@@ -87,7 +87,6 @@ public class FeatureExenseStep extends CFWAppFeature {
 		CFW.Registry.Widgets.add(new WidgetSchedulerStatusByProject());
 		CFW.Registry.Widgets.add(new WidgetSchedulerStatusByUsers());
 		CFW.Registry.Widgets.add(new WidgetSchedulerStatusByCurrentUser());
-		CFW.Registry.Widgets.add(new WidgetSchedulerStatusAll());
 		CFW.Registry.Widgets.add(new WidgetSchedulerExecutionsLast());
 		CFW.Registry.Widgets.add(new WidgetSchedulerExecutionsTimerange());
 		CFW.Registry.Widgets.add(new WidgetSchedulerDurationChart());
